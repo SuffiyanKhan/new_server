45714ed5868d271b23f291cf0b9529420faea5d8
Suffiyan Khan <suffiyanahmed804092@gmai.com>
2024-07-31 00:47:36 +0500

    working

--- src/modules/certificate.module.js
+++ src/modules/certificate.module.js
@@ -1,8 +1,32 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const { Schema } = mongoose;
+// const { Schema } = mongoose;
 
-const UserSchema = new Schema({
+// const UserSchema = new Schema({
+//     name: { type: String, required: true },
+//     course: { type: String, required: true },
+//     date: { type: Date, required: true },
+//     certificateUrl: { type: String, required: true },
+//     email: { type: String, required: true },
+//     cnic: { type: String, default: null },
+//     batchNo: { type: String, default: null },
+//     rollno:{type:String,default:null },
+//     isEmail: {
+//         type: Boolean,
+//         default: false
+//     }
+// }, { timestamps: true })
+
+// const Certificatie = mongoose.model('AllCertificate', UserSchema);
+
+// export default Certificatie;
+
+import mongoose, { mongo } from "mongoose";
+
+const {Schema}=mongoose;
+
+
+const certificateSchema = new Schema({
     name: { type: String, required: true },
     course: { type: String, required: true },
     date: { type: Date, required: true },
@@ -10,14 +34,17 @@ const UserSchema = new Schema({
     email: { type: String, required: true },
     cnic: { type: String, default: null },
     batchNo: { type: String, default: null },
-    rollno:{type:String,default:null },
+    rollno: { type: String, default: null },
     isEmail: {
         type: Boolean,
         default: false
     }
-}, { timestamps: true })
+},{timeseries:true})
 
-const Certificatie = mongoose.model('AllCertificate', UserSchema);
 
-export default Certificatie;
+const certificateModule = mongoose.model("AllCertificate",certificateSchema);
+
+
+export default certificateModule
+
 
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 import AdminPassword from "./adminpassword.model.js";
-import Certificatie from "./certificate.module.js";
+// import Certificatie from "./certificate.module.js";
 import CoursesModel from "./courses.model.js";
 import StudentsModel from "./students.model.js";
-
+import certificateModule from "./certificate.module.js"
 
 let db = {}
 
 db.mongoose = mongoose;
-db.certificate = Certificatie;
+// db.certificate = certificateModel
+db.certificate = certificateModule;
 db.adminpassword = AdminPassword;
 db.courses = CoursesModel
 db.students = StudentsModel
