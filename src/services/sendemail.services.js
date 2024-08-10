d3bb921dcba3cb64b10e820f87f307e4e8097481
Suffiyan Khan <suffiyanahmed804092@gmai.com>
2024-08-10 08:47:07 +0500

    working

--- src/services/sendemail.services.js
+++ src/services/sendemail.services.js
@@ -10,8 +10,8 @@ import { serverConfig } from '../configs/server.config.js';
 
 const { certificate: Certificate, students: Students } = db
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // const pdfDir = path.join(__dirname, 'temp/pdfs');
 const pdfDirectory = path.join(__dirname, 'services', 'temp', 'pdfs');
